99a7c9b494fd01d2209c663756b79ce96051c032
Martin Rio <martin@ombuweb.com>
2012-06-13 10:41:44 -0700

    Removes Redmine setup code that went into Redmine's fabfile

--- fiber/profiles/redmine/__init__.py
+++ fiber/profiles/redmine/__init__.py
@@ -7,7 +7,7 @@ conf = {
     'version': '1.4.1',
     'path':    '/home/ombu/redmine',
     'repo':    'git://github.com/redmine/redmine.git',
-    'site_name':    'rm.ombuweb.com'
+    'site_name':    't.ombuweb.com'
 }
 conf = _AttributeDict(conf)
 
@@ -26,29 +26,10 @@ def install(server):
     sudo('gem install --no-ri --no-rdoc bundler')
 
     # Setup environment
-    run('rm -rf %s && git clone %s %s' % (conf.path, conf.repo, conf.path))
-    run('cd %s && git checkout %s' % (conf.path, conf.version))
     upload_template(
         'fiber/profiles/redmine/templates/passenger.conf',
         '/etc/apache2/mods-available/passenger.conf',
         use_sudo=True)
-    upload_template(
-        'fiber/profiles/redmine/templates/apache_vhost',
-        '/etc/apache2/sites-available/redmine',
-        context=conf,
-        use_sudo=True)
-    _set_app_permissions()
-    sudo('cd %s && bundle install --without development test' % conf.path)
-    sudo('a2ensite redmine && service apache2 stop && service apache2 start')
-
-    # Configure Redmine
-    _config()
-
-def _config():
-    pass
-
-def _set_app_permissions():
-    run('chmod a+x %s/public' % conf.path)
 
 def _upgrade_rubygems():
     sudo('gem install rubygems-update')
